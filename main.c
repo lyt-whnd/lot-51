bda98788e5de9d56fc405016befed7a8cd93ec72
yutong li <502117901@qq.com>
2020-08-05 10:58:10 +0800

    初步完结

--- main.c
+++ main.c
@@ -3,14 +3,32 @@
 #include <stdio.h>
 #define uchar unsigned char
 #define uint unsigned int
+sbit mode=P2^0;			//mode=0为按键和WiFi控制  mode=1为传感器控制
+sbit S1=P1^7;
+sbit S2=P1^3;
 sbit light=P2^1;
-sbit key1=P3^1;
-sbit key2=P3^2;
+sbit key1=P3^2;    //按键3
+sbit key2=P3^3;		 //按键4		
 
 sbit DIN=P3^4;
 sbit CS=P3^5;
 sbit DCLK=P3^6;
 sbit DOUT=P3^7;
+
+sbit EN=P2^7;
+sbit RS=P2^6;
+sbit lcdWR=P2^5;
+
+uchar code table1[]="loading...";
+uchar code table2[]="OK";
+uchar code table3[]="status";
+uchar code table4[]="Light";
+uchar code table5[]="low";
+uchar code table6[]="high";
+uchar code table7[]="on";
+uchar code table8[]="off";
+void lcdWriteCom(uchar dat);
+void lcdString(uchar *str,uint length);
 /*延时ms函数*/
 void delayms(uint z)
 {
@@ -80,12 +98,12 @@ uint adRead()
 	return dat;
 }
 //命令转换操作
-uint adReadData(uchar dat)		//光敏电阻为 0xc4
+uint adReadData()		//光敏电阻为 0xc4
 {
 	uint i,AD_value;
 	DCLK=0;
 	CS=0;
-	adWrite(dat);
+	adWrite(0xc4);
 	for(i=6;i>0;i--);
 	DCLK=1;
 	_nop_();
@@ -98,9 +116,9 @@ uint adReadData(uchar dat)		//光敏电阻为 0xc4
 	return AD_value;
 }
 //封装开关
-void adSwitch(uchar dat)
+void adSwitch()
 {
-	if(adReadData(dat)<200)
+	if(adReadData()<200)
 		switchLight(1);
 	else
 		switchLight(2);
@@ -121,17 +139,24 @@ void wifiInit()
 //设置WiFi打开端口号
 void wifiConfig()
 {
+	//液晶显示正在设置
+	lcdWriteCom(0x80+0x02);
+	lcdString(table1,10);
+	
 	printf("AT+CWMODE+1");
 	delayms(1000);
 	printf("AT+RST");
 	delayms(1000);
-	printf("AT+CWJAP=\"XXX\",\"XXX\"");
+	printf("AT+CWJAP=\"XXX\",\"XXX\"");		//此处为要连接的WIFI名称和密码
 	delayms(1000);
 	printf("AT+CIPMUX=1");
 	delayms(1000);
 	printf("AT+CIPSERVER=1,8080");
 	delayms(1000);
-	//to do液晶显示设置成功
+	//液晶显示设置成功
+	lcdWriteCom(0x01);
+	lcdWriteCom(0x80);
+	lcdString(table2,2);
 }
 //封装开关
 void wifiSwitch()
@@ -144,3 +169,97 @@ void wifiSwitch()
 		switchLight(2);
 }
 /*液晶实时状态回显*/
+//液晶写命令
+void lcdWriteCom(uchar dat)
+{
+	RS=0;
+	lcdWR=0;
+	P0=dat;
+	delayms(5);
+	EN=1;
+	delayms(5);
+	EN=0;
+}
+//液晶写数据
+void lcdWriteData(uchar dat)
+{
+	RS=1;
+	lcdWR=0;
+	P0=dat;
+	delayms(5);
+	EN=1;
+	delayms(5);
+	EN=0;
+}
+//初始化液晶
+void lcdInit()
+{
+	lcdWriteCom(0x38);
+	lcdWriteCom(0x0c);
+	lcdWriteCom(0x06);
+	lcdWriteCom(0x01);
+}
+//字符串显示函数
+void lcdString(uchar *str,uint length)
+{
+	uint i;
+	for(i=0;i<length;i++)
+	{
+		lcdWriteData(str[i]);
+	}
+}
+//光强显示
+void lcdLight()
+{
+	lcdWriteCom(0x80+0x40);
+	lcdString(table4,5);
+	lcdWriteCom(0x80+0x46);
+	if(adReadData()<200)
+		lcdString(table5,3);
+	else
+		lcdString(table6,4);
+}
+//液晶显示灯的状态变化
+void lcdStatus()
+{
+	lcdWriteCom(0x80);
+	if(light==0)
+		lcdString(table7,2);
+	if(light==1)
+		lcdString(table8,3);
+}
+/*模式设置*/
+void modeConfig()
+{
+	if(S1==0)
+	{
+		delayms(10);
+		if(S1==0)
+		{
+			mode=~mode;
+		}
+		while(!S1);
+	}
+	if(mode==1)
+	{
+		adSwitch();
+	}
+}
+/*主函数*/
+void main()
+{
+	wifiInit();
+	lcdInit();
+	wifiConfig();
+	mode=0;
+	S1=1;
+	S2=0;
+	while(1)
+	{
+		lcdStatus();
+		lcdLight();
+		keyControl();
+		wifiSwitch();
+		modeConfig();
+	}
+}
